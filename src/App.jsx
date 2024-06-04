f3f2cb4b1aa572c25739bd95f2e11519285a756d
Rahul2783 <sahurahul2783@gmail.com>
2024-06-04 16:03:52 +0530

    .

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+import HeroSection from "./components/Hero";
+import Navigation from "./components/Navigation";
+
+const App = ( ) => {
+    return (
+    <div>
+       <Navigation />
+       <HeroSection />
+    </div>
+    );
+};
+export default App;
---
+++ src/components/Navigation.jsx
@@ -0,0 +1,20 @@
+const Navigation =()=>{
+    return(
+        <nav className="container">
+        <div className="logo">
+            <img src="logo.png" width={100} height={72} alt="" />
+        </div>
+        <ul>
+            <li href="#">Menu</li>
+            <li href="#">Location</li>
+            <li href="#">About</li>
+            <li href="#">Contact</li>
+        </ul>
+        <button>Login</button>
+        
+
+        </nav> 
+    );
+};
+
+export default Navigation;
